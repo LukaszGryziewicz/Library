bb3951cdf84544bc84882e5cf5b412af9f8a97d3
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-06-10 11:08:47 +0200

    Added Cors in WebConfiguration.java

--- src/main/java/com/library/LibraryApplication.java
+++ src/main/java/com/library/LibraryApplication.java
@@ -3,16 +3,28 @@ package com.library;
 import com.library.book.Book;
 import com.library.book.BookController;
 import com.library.book.BookService;
+import org.apache.catalina.filters.CorsFilter;
+import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class LibraryApplication {
+
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(LibraryApplication.class, args);
 	}
+
+
+
 
 
 }
