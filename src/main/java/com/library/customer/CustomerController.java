534840fdad5709eff23fc5d07ca14409eb4febc0
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-06-11 11:14:15 +0200

    added CustomerController

--- src/main/java/com/library/customer/CustomerController.java
+++ src/main/java/com/library/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.library.customer;
 
+import com.library.book.Book;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping ("/customer")
+@RequestMapping("/customer")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -17,20 +18,32 @@ public class CustomerController {
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<Customer>> getAllCustomers(){
+    public ResponseEntity<List<Customer>> getAllCustomers() {
         final List<Customer> customers = customerService.getCustomers();
         return new ResponseEntity<>(customers, HttpStatus.OK);
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Customer>addCustomer(@RequestBody Customer customer){
-        final Customer newCustomer=customerService.addCustomer(customer);
-        return new ResponseEntity<>(newCustomer,HttpStatus.CREATED);
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
+        final Customer newCustomer = customerService.addCustomer(customer);
+        return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/delete")
-    public ResponseEntity<?>deleteCustomer( Customer customer){
-        customerService.deleteCustomer(customer);
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Customer> findCustomer(@PathVariable("id") Long id) {
+        Customer customer = customerService.findCustomer(id);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("id") Long id, @RequestBody Customer customer) {
+        Customer updatedCustomer = customerService.updateCustomer(id, customer);
+        return new ResponseEntity<>(updatedCustomer, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable("id") Long id) {
+        customerService.deleteCustomer(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
