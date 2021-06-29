b46dd43475f1ffb632886fa815466709a03b0329
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-06-29 20:05:43 +0200

    refactored customer controller tests

--- src/test/java/com/library/customer/CustomerControllerTest.java
+++ src/test/java/com/library/customer/CustomerControllerTest.java
@@ -1,60 +1,119 @@
 package com.library.customer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.List;
+import javax.transaction.Transactional;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(CustomerController.class)
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
 public class CustomerControllerTest {
 
-    @MockBean
-    private CustomerService customerService;
 
     @Autowired
     private MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private CustomerService customerService;
 
     @Test
     void shouldReturnAllCustomers() throws Exception {
         //given
         Customer customer = new Customer("Adam", "Dominik");
         Customer customer2 = new Customer("Łukasz", "Gryziewicz");
-        //when
-        when(customerService.getCustomers()).thenReturn(List.of(customer, customer2));
-        //then
+        customerService.addCustomer(customer);
+        customerService.addCustomer(customer2);
+        //expect
         mockMvc.perform(MockMvcRequestBuilders.get("/customer"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].firstName").value("Adam"))
-                .andExpect(jsonPath("$[0].lastName").value("Dominik"))
-                .andExpect(jsonPath("$[1].firstName").value("Łukasz"))
-                .andExpect(jsonPath("$[1].lastName").value("Gryziewicz"));
+                .andExpect(jsonPath("$[0].firstName").value(customer.getFirstName()))
+                .andExpect(jsonPath("$[0].lastName").value(customer.getLastName()))
+                .andExpect(jsonPath("$[1].firstName").value(customer2.getFirstName()))
+                .andExpect(jsonPath("$[1].lastName").value(customer2.getLastName()));
     }
+
+    @Test
+    void shouldAddCustomer() throws Exception {
+        //when
+        Customer customer = new Customer("Adam", "Dominik");
+        String content = objectMapper.writeValueAsString(customer);
+        //expect
+        mockMvc.perform(MockMvcRequestBuilders.post("/customer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName").value(customer.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(customer.getLastName()));
+    }
+
 
     @Test
     void shouldReturnCustomerWithGivenId() throws Exception {
         //given
         Customer customer = new Customer("Adam", "Dominik");
-        customer.setId(1);
-        //when
-        when(customerService.findCustomer(customer.getId())).thenReturn(customer);
-        //than
-        mockMvc.perform(MockMvcRequestBuilders.get("/customer/1"))
+        customerService.addCustomer(customer);
+        //expect
+        mockMvc.perform(MockMvcRequestBuilders.get("/customer/" + customer.getId()))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.firstName").value("Adam"))
-                .andExpect(jsonPath("$.lastName").value("Dominik"));
-
+                .andExpect(jsonPath("$.firstName").value(customer.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(customer.getLastName()));
     }
 
+    @Test
+    void shouldFindCustomerById() throws Exception {
+        //given
+        Customer customer = new Customer("Adam", "Dominik");
+        customerService.addCustomer(customer);
+        //expect
+        mockMvc.perform(MockMvcRequestBuilders.get("/customer/" + customer.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value(customer.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(customer.getLastName()));
+    }
+
+    @Test
+    void shouldUpdateCustomer() throws Exception {
+        //given
+        Customer customer = new Customer("Adam", "Dominik");
+        Customer customer2 = new Customer("Łukasz", "Gryziewicz");
+        customerService.addCustomer(customer);
+        String content = objectMapper.writeValueAsString(customer2);
+        //expect
+        mockMvc.perform(MockMvcRequestBuilders.put("/customer/update/" + customer.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value(customer2.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(customer2.getLastName()));
+    }
+
+    @Test
+    void shouldDeleteCustomer() throws Exception {
+        //given
+        Customer customer = new Customer("Adam", "Dominik");
+        customerService.addCustomer(customer);
+        //expect
+        mockMvc.perform(MockMvcRequestBuilders.delete("/customer/delete/" + customer.getId()))
+                .andExpect(status().isOk());
+
+    }
 }
 
