9928ad072395b290d2c0c578f30b126de12eed81
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-07-01 19:25:58 +0200

    added more tests to RentalControllerTest

--- src/main/java/com/library/rental/RentalController.java
+++ src/main/java/com/library/rental/RentalController.java
@@ -4,13 +4,11 @@ import com.library.exceptions.ExceededMaximumNumberOfRentalsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@EnableWebMvc
 @RequestMapping("/rental")
 public class RentalController {
     private final RentalService rentalService;
@@ -26,20 +24,20 @@ public class RentalController {
     }
 
     @GetMapping("/customerRentals/{id}")
-    public ResponseEntity<List<Rental>> getRentalsOfCustomer(Long id) {
+    public ResponseEntity<List<Rental>> getRentalsOfCustomer(@PathVariable("id") Long id) {
         final List<Rental> rentals = rentalService.getRentalsOfCustomer(id);
         return new ResponseEntity<>(rentals, HttpStatus.OK);
     }
 
     @GetMapping("/bookRentals/{id}")
-    public ResponseEntity<List<Rental>> getRentalsOfBook(Long id) {
+    public ResponseEntity<List<Rental>> getRentalsOfBook(@PathVariable("id") Long id) {
         final List<Rental> rentalsOfBook = rentalService.getRentalsOfBook(id);
         return new ResponseEntity<>(rentalsOfBook, HttpStatus.OK);
     }
 
     @PostMapping("/{customerId}/{title}/{author}")
     public ResponseEntity<Rental> addNewRental(@PathVariable("customerId") Long customerId, @PathVariable("title") String title, @PathVariable("author") String author) throws ExceededMaximumNumberOfRentalsException {
-        final Rental newRental = rentalService.createRental(customerId, title, author, LocalDateTime.now());
+        final Rental newRental = rentalService.rent(customerId, title, author, LocalDateTime.now());
         return new ResponseEntity<>(newRental, HttpStatus.CREATED);
     }
 
@@ -51,7 +49,7 @@ public class RentalController {
 
     @PutMapping("/endRental/{id}")
     public ResponseEntity<Rental> endRental(@PathVariable("id") Long id) {
-        rentalService.endRental(id, LocalDateTime.now());
+        rentalService.returnBook(id, LocalDateTime.now());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
