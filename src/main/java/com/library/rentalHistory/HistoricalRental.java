7900c9b617c29194ccd9aadf7d2e179265a32b64
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-08-05 20:41:55 +0200

    changed HistoricalRental entity

--- src/main/java/com/library/rentalHistory/HistoricalRental.java
+++ src/main/java/com/library/rentalHistory/HistoricalRental.java
@@ -1,6 +1,7 @@
 package com.library.rentalHistory;
 
 import javax.persistence.*;
+import java.time.Instant;
 
 @Entity
 public class HistoricalRental {
@@ -8,32 +9,69 @@ public class HistoricalRental {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
     private long id;
+    private String historicalRentalId;
+    private Instant dateCreated;
+    private Instant dateEnded;
     //book
+    private String bookId;
     private String title;
     private String author;
     private String isbn;
     //customer
+    private String customerId;
     private String firstName;
     private String lastName;
 
     public HistoricalRental() {
     }
 
-    public HistoricalRental(String title, String author, String isbn, String firstName, String lastName) {
+    public HistoricalRental(String historicalRentalId, Instant dateCreated, Instant dateEnded, String bookId, String title, String author, String isbn, String customerId, String firstName, String lastName) {
+        this.historicalRentalId = historicalRentalId;
+        this.dateCreated = dateCreated;
+        this.dateEnded = dateEnded;
+        this.bookId = bookId;
         this.title = title;
         this.author = author;
         this.isbn = isbn;
+        this.customerId = customerId;
         this.firstName = firstName;
         this.lastName = lastName;
-
     }
 
     public long getId() {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public String getHistoricalRentalId() {
+        return historicalRentalId;
+    }
+
+    public void setHistoricalRentalId(String historicalRentalId) {
+        this.historicalRentalId = historicalRentalId;
+    }
+
+    public Instant getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Instant dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Instant getDateEnded() {
+        return dateEnded;
+    }
+
+    public void setDateEnded(Instant dateEnded) {
+        this.dateEnded = dateEnded;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
     }
 
     public String getTitle() {
@@ -60,6 +98,10 @@ public class HistoricalRental {
         this.isbn = isbn;
     }
 
+    public String getCustomerId() {
+        return customerId;
+    }
+
     public String getFirstName() {
         return firstName;
     }
@@ -75,6 +117,5 @@ public class HistoricalRental {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
 }
 
