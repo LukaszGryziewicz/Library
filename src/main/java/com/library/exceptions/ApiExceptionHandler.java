2bbb6169f351a2ae74cfb9b84b3bce783d05f4dc
LukaszGryziewicz <lukaszgryziewicz@gmail.com>
2021-09-15 19:59:56 +0200

    changed http statuses for exceptionHandler

--- src/main/java/com/library/exceptions/ApiExceptionHandler.java
+++ src/main/java/com/library/exceptions/ApiExceptionHandler.java
@@ -10,22 +10,22 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
 
     @ExceptionHandler(value = {CustomerNotFoundException.class, BookNotFoundException.class, RentalNotFoundException.class})
     ResponseEntity<Object> handleNotFoundExceptions(Exception e) {
-        ApiException apiException = new ApiException(e.getMessage(), NOT_FOUND, now());
-        return new ResponseEntity<>(apiException, NOT_FOUND);
+        ApiException apiException = new ApiException(e.getMessage(), BAD_REQUEST, now());
+        return new ResponseEntity<>(apiException, BAD_REQUEST);
     }
 
     @ExceptionHandler(NoBookAvailableException.class)
     ResponseEntity<Object> handleNoBookAvailableException(NoBookAvailableException e) {
-        ApiException apiException = new ApiException(e.getMessage(), NOT_FOUND, now());
-        return new ResponseEntity<>(apiException, NOT_FOUND);
+        ApiException apiException = new ApiException(e.getMessage(), BAD_REQUEST, now());
+        return new ResponseEntity<>(apiException, BAD_REQUEST);
     }
 
     @ExceptionHandler(ExceededMaximumNumberOfRentalsException.class)
